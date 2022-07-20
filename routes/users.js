17b81c455265b788b56055eeb0bee2a6d1cf7663
StingerSan <stinger@int.pl>
2022-07-20 19:50:10 +0200

    Configuring multer in-depth

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const multer = require("multer");
 
-const upload = multer({});
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
 router.get("/", function (req, res) {
@@ -15,6 +24,8 @@ router.get("/new-user", function (req, res) {
 router.post("/profiles", upload.single("image"), function (req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
+
+  res.redirect("/");
 });
 
 module.exports = router;
